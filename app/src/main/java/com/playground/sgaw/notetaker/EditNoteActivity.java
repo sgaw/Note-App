ac0396cfe286d2dee3dc1c2bf336df1a1388c261
Shirley G <shirley.gaw@gmail.com>
2015-01-28 15:50:07 +0100

    App without fragments, reloads all files on startup, no tests.

---
+++ app/src/main/java/com/playground/sgaw/notetaker/EditNoteActivity.java
@@ -0,0 +1,147 @@
+package com.playground.sgaw.notetaker;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import junit.framework.Assert;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class EditNoteActivity extends ActionBarActivity {
+    public static final String EXTRA_MESSAGE = "com.playground.sgaw.notetaker.MESSAGE";
+    public static final String EXTRA_FILENAME = "com.playground.sgaw.notetaker.FILENAME";
+
+    private static final String LOG_TAG = EditNoteActivity.class.getCanonicalName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_note);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Check if we are editing an existing note.
+        String contents = getIntent().getStringExtra(EXTRA_MESSAGE);
+        if (contents != null) {
+            Log.i(LOG_TAG, "Existing note being edited: " + contents);
+            EditText editText = (EditText) findViewById(R.id.editNoteText);
+            editText.setText(contents);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_edit_note, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        // TODO(sgaw): Auto-save if Up button clicked
+        switch(item.getItemId()) {
+            case R.id.action_confirm:
+                onSave();
+                return true;
+            case R.id.action_delete:
+                onDelete();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /**
+     * Implements save action.  Extracts note contents and saves state.  Returns to home screen
+     * view.
+     */
+    private void onSave() {
+        Log.i(LOG_TAG, "openSave()");
+        EditText editText = (EditText) findViewById(R.id.editNoteText);
+        String message = editText.getText().toString();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        if (message != null && !message.isEmpty()) {
+            /* TODO(sgaw): If large number of notes are expected, replace file store with
+             database entries */
+            try {
+
+                String filename = getIntent().getStringExtra(EXTRA_FILENAME);
+                // Re-write note contents to existing files if possible.
+                if (filename == null) {
+                    saveNoteContents(message);
+                } else {
+                    saveNoteContents(message, filename);
+                }
+
+                startActivity(intent);
+            } catch (IOException ioe) {
+                // Warn user that no save completed
+                Toast.makeText(this, R.string.save_fail_text, Toast.LENGTH_SHORT)
+                        .show();
+                Log.e(LOG_TAG, "onSave", ioe);
+                return;
+            }
+        }
+    }
+
+    /**
+     * Write contents of note to a new file.
+     *
+     * @param contents the note contents to save to a file.
+     * @throws IOException
+     */
+    private void saveNoteContents(String contents) throws IOException {
+        // Use timestamp to name files
+        String filename = "Note-" + System.currentTimeMillis() + ".txt";
+        saveNoteContents(contents, filename);
+    }
+
+    /**
+     * Write contents of note to the specified file (internal app storage).
+     * @param contents the note contents to save to a file.
+     * @param filename the name of a file to overwrite or create.
+     * @throws IOException
+     */
+    private void saveNoteContents(String contents, String filename) throws IOException {
+        Log.i(LOG_TAG, "Saving to filename: " + filename);
+        FileOutputStream fileOutputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+        fileOutputStream.write(contents.getBytes());
+        fileOutputStream.close();
+    }
+
+    /**
+     * Implements delete action.  Removes underlying file representing note contents.  Return
+     * to home screen view.
+     *
+     * If the note was only a draft, the contents are discarded.
+     */
+    private void onDelete() {
+        Log.i(LOG_TAG, "onDelete()");
+        Intent callingIntent = getIntent();
+        final String filename = callingIntent.getStringExtra(EXTRA_FILENAME);
+        // TODO(sgaw): Add confirmation of delete?
+        if (filename != null && !filename.isEmpty()) {
+            Log.i(LOG_TAG, "Deleting note: " + filename);
+            this.deleteFile(filename);
+            Toast.makeText(this, R.string.delete_text, Toast.LENGTH_SHORT).show();
+        }
+
+        Log.i(LOG_TAG, "onDelete navigating home");
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/playground/sgaw/notetaker/MainActivity.java
@@ -0,0 +1,195 @@
+package com.playground.sgaw.notetaker;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+
+
+public class MainActivity extends ActionBarActivity {
+    private static final String LOG_TAG = MainActivity.class.getCanonicalName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Read notes from files created by this app, each note corresponding to one file.
+        // TODO(sgaw): Differentiate between app startup and resume
+        Log.i(LOG_TAG, "No saved note");
+        displayNotes();
+
+        // Show something if there are no notes yet.
+        LinearLayout notesLayout = (LinearLayout) findViewById(R.id.notes_container);
+        if (notesLayout.getChildCount() == 0) {
+            TextView stubTextView = new TextView(this);
+            stubTextView.setText(R.string.empty_notes_text);
+            notesLayout.addView(stubTextView);
+        }
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_create) {
+            createNote();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Action handler for creating a new note.  Passes to the EditNoteActivity.
+     */
+    public void createNote() {
+        Log.i(LOG_TAG, "createNote");
+
+        Intent intent = new Intent(this, EditNoteActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Add all saved notes to home screen.
+     */
+    private void displayNotes() {
+        Log.i(LOG_TAG, "displayNotes");
+        String[] files = getFilesDir().list();
+        // TODO(sgaw): Make file reading asynchronous callbacks
+        // TODO(sgaw): sort by modification time and show only the most recent N
+        for (String filename : files) {
+            Log.i(LOG_TAG, "Filename: " + filename);
+            // Read note from file, dropping inaccessible notes
+            String contents = readNote(filename);
+            if (contents != null) {
+                addNote(filename, contents);
+            }
+        }
+    }
+
+    /**
+     * Add a note to list on home screen for browsing.
+     * @param filename the underlying file for saving note contents.
+     * @param contents the note contents.
+     */
+    private void addNote(String filename, String contents) {
+        Log.i(LOG_TAG, "addNote");
+        NoteTextView noteTextView = new NoteTextView(this, filename);
+        noteTextView.setText(contents);
+
+        Log.i(LOG_TAG, "Created noteTextView " + noteTextView.toString());
+        LinearLayout notesLayout = (LinearLayout) findViewById(R.id.notes_container);
+        notesLayout.addView(noteTextView);
+    }
+
+    /**
+     * Read contents of note from file.
+     *
+     * Returns null if there are I/O issues.
+     */
+    private String readNote(String filename) {
+        Log.i(LOG_TAG, "readNote, filename:" + filename);
+        try {
+            StringBuffer stringBuffer = new StringBuffer("");
+
+            StringBuilder builder = new StringBuilder();
+            BufferedReader bufferedReader = new BufferedReader(
+                    new FileReader(getFilesDir() + "/" + filename));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                Log.i(LOG_TAG, line);
+                builder.append(line);
+                builder.append("\n");
+            }
+            bufferedReader.close();
+            return builder.toString();
+        } catch (FileNotFoundException fnfe) {
+            Log.e(LOG_TAG, "Unable to open file: " + filename, fnfe);
+            return null;
+        } catch (IOException ioe) {
+            Log.e(LOG_TAG, "Unable to read file: " + filename, ioe);
+            return null;
+        }
+    }
+
+    /**
+     * TextView to display notes that stores the underlying note filename.
+     *
+     * Navigates to the note editor when the user clicks on a note.
+     */
+    private class NoteTextView extends TextView {
+        private final String filename;
+        private String previewText;
+
+        public NoteTextView(Context context, String filename) {
+            super(context);
+            this.filename = filename;
+
+            // Navigate to note editor when user clicks on a note.
+            Log.i(LOG_TAG, "NoteTextView.setClickListener");
+            this.setOnClickListener(new OnClickListener() {
+                /**
+                 * Send an intent to the EditNoteActivity with the contents of this note and
+                 * the underlying file that must be edited.
+                 * @param v the NoteTextView that was clicked.
+                 */
+                @Override
+                public void onClick(View v) {
+                    Log.i(LOG_TAG, "NoteTextView.OnClickListener.onClick");
+                    Intent intent = new Intent(v.getContext(), EditNoteActivity.class);
+                    NoteTextView textView = (NoteTextView) v;
+                    intent.putExtra(EditNoteActivity.EXTRA_MESSAGE, textView.getText().toString());
+                    intent.putExtra(EditNoteActivity.EXTRA_FILENAME, textView.filename);
+                    Log.i(LOG_TAG, "EXTRA_MESSAGE: "
+                            + intent.getStringExtra(EditNoteActivity.EXTRA_MESSAGE));
+                    startActivity(intent);
+                }
+            });
+
+            // TODO(sgaw): Read note asynchronously showing just preview text in case of long notes
+        }
+
+        @Override
+        public String toString() {
+            return "Filename ("+ filename + ")\nMessage(" + getText().toString() + ")";
+        }
+    }
+}
